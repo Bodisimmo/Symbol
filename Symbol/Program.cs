3e26af432d18c0356e1bb139f727abdb8d943d4d
Uncle <Uncle@DESKTOP-EO0ENMJ>
2022-07-16 21:41:42 +0400

    Добавьте файлы проекта.

---
+++ Symbol/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Symbol
+{
+    public static void Main()
+    {
+        int x = 0;
+        int y = 0;
+
+        Console.Write('*');
+
+        do
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow: y--; break;
+                case ConsoleKey.DownArrow: y++; break;
+                case ConsoleKey.RightArrow: x++; break;
+                case ConsoleKey.LeftArrow: x--; break;
+                case ConsoleKey.Escape: return;
+            }
+
+            if (x < 0)
+                x = 0;
+            else if (x >= Console.BufferWidth)
+                x = Console.BufferWidth - 0;
+
+            if (y < 0)
+                y = 0;
+            else if (y >= Console.BufferHeight)
+                y = Console.BufferHeight - 0;
+
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.Write('*');
+
+        } 
+        while (true);
+
+    }
+
+}
